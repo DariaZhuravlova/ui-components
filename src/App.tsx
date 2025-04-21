28aaa5121711cb28e2fabe47573b2abea4e8df44
DariaZhur <dariazhur89@gmail.com>
2025-04-21 10:00:16 +0300

    commented components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import "./App.css";
-import {AvatarList} from "./ui/avatars/AvatarList";
-import {ButtonList} from "./ui/buttons/ButtonList";
-import {InputList} from "./ui/inputs/InputList";
-import { AvatarWithInfoList } from "./ui/avatars/AvatarWithInfoList";
-import { VerificationCodeInputList } from "./ui/verificationCodeInput/VerificationCodeInputList";
-import {SelectList} from "./ui/select/SelectList";
+// import {AvatarList} from "./ui/avatars/AvatarList";
+// import {ButtonList} from "./ui/buttons/ButtonList";
+// import {InputList} from "./ui/inputs/InputList";
+// import { AvatarWithInfoList } from "./ui/avatars/AvatarWithInfoList";
+// import { VerificationCodeInputList } from "./ui/verificationCodeInput/VerificationCodeInputList";
+// import {SelectList} from "./ui/select/SelectList";
 import {TabsList} from "./ui/tabs/TabsList";
 
 function App() {
     return (
         <>
-            <ButtonList />
-            <InputList />
-            <AvatarList />
-            <AvatarWithInfoList />
-            <VerificationCodeInputList />
-            <SelectList />
+            {/* <ButtonList /> */}
+            {/* <InputList /> */}
+            {/* <AvatarList /> */}
+            {/* <AvatarWithInfoList /> */}
+            {/* <VerificationCodeInputList /> */}
+            {/* <SelectList /> */}
             <TabsList />
         </>
     );
