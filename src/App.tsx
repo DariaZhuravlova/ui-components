923b8a9090466def3ef695513057a1a117e7d3a5
DariaZhur <dariazhur89@gmail.com>
2025-04-23 19:49:43 +0300

    add comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import "./App.css";
-import {AvatarList} from "./ui/avatars/AvatarList";
-import {ButtonList} from "./ui/buttons/ButtonList";
-import {InputList} from "./ui/inputs/InputList";
-import { AvatarWithInfoList } from "./ui/avatars/AvatarWithInfoList";
-import { VerificationCodeInputList } from "./ui/verificationCodeInput/VerificationCodeInputList";
-import {SelectList} from "./ui/select/SelectList";
+// import {AvatarList} from "./ui/avatars/AvatarList";
+// import {ButtonList} from "./ui/buttons/ButtonList";
+// import {InputList} from "./ui/inputs/InputList";
+// import { AvatarWithInfoList } from "./ui/avatars/AvatarWithInfoList";
+// import { VerificationCodeInputList } from "./ui/verificationCodeInput/VerificationCodeInputList";
+// import {SelectList} from "./ui/select/SelectList";
 import {TabsList} from "./ui/tabs/TabsList";
 
 function App() {
     return (
         <>
-            <ButtonList />
-            <InputList />
-            <AvatarList />
-            <AvatarWithInfoList />
-            <VerificationCodeInputList />
+            {/* <ButtonList /> */}
+            {/* <InputList /> */}
+            {/* <AvatarList /> */}
+            {/* <AvatarWithInfoList /> */}
+            {/* <VerificationCodeInputList /> */}
 
             <TabsList />
-            <SelectList />
+            {/* <SelectList /> */}
         </>
     );
 }
